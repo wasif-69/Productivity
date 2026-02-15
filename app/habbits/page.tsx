ecd2e57749b6a3a24f8a999eb04f41d761c47675
wasif-69 <wasifkhalil69@gmail.com>
2026-02-15 09:54:33 +0500

    adding more

--- app/habbits/page.tsx
+++ app/habbits/page.tsx
@@ -43,10 +43,10 @@ export default function Page() {
 
   const habbitList = [
     { name: "Books", minimum: "1 hour study", points: 15},
-    { name: "Admissions", minimum: "Turkey Universities", points: 15 },
-    { name: "Electronics", minimum: " book print and diode and next video record ", points: 30 },
-    { name: "AI", minimum: " AI mate chatting feathure final and LLM ", points: 30},
-    { name: "A Level Math", minimum: "Vectors Past Papers ", points: 25 },
+    { name: "Admissions", minimum: "Turkey Universities", points: 5 },
+    { name: "Electronics", minimum: "  diode and next video set ", points: 40 },
+    { name: "AI", minimum: " AI mate chatting feathure final and LLM ", points: 40},
+    // { name: "A Level Math", minimum: "Vectors Past Papers ", points: 25 },
   ];
 
   const getDocsFireStore = async () => {
